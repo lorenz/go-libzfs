c9694e026c075f5bdd491f717d7369c966ef471e
Lorenz Brun <lorenz@dolansoft.org>
2018-09-03 00:16:37 +0200

    Fix path for zpool_test

--- zpool_test.go
+++ zpool_test.go
@@ -6,7 +6,7 @@ import (
 	"os"
 	"testing"
 
-	zfs "github.com/bicomsystems/go-libzfs"
+	zfs "github.com/lorenz/go-libzfs"
 )
 
 /* ------------------------------------------------------------------------- */
